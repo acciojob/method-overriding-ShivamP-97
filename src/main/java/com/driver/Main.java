b27c9a9ed5fd826a99b4a4d6d317a3e5482a77e0
= <shivampathak19972@gmail.com>
2026-01-15 22:29:10 +0530

    Test

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,8 @@
+package com.driver;
+
 public class Main {
     public static void main(String[] args) {
-
-        // Create object of B
         B obj = new B();
-
-        // Call overridden method
         System.out.println(obj.meth());
     }
 }
