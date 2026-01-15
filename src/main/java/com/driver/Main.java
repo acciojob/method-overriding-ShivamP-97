4d4e515183a6cf8c595b98fca4ad6a99837e66ff
= <shivampathak19972@gmail.com>
2026-01-15 22:31:45 +0530

    Test

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,22 @@
 package com.driver;
 
 public class Main {
+
+    // Class A
+    static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    // Class B extends A
+    static class B extends A {
+        @Override
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
     public static void main(String[] args) {
         B obj = new B();
         System.out.println(obj.meth());
