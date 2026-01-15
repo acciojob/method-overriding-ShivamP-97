08d3d811a8e33c39f573f82b12e3b9ade5b53aaf
= <shivampathak19972@gmail.com>
2026-01-15 22:26:23 +0530

    Test

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
-package com.driver;
-
 public class Main {
-  
+    public static void main(String[] args) {
+
+        // Create object of B
+        B obj = new B();
+
+        // Call overridden method
+        System.out.println(obj.meth());
+    }
 }
